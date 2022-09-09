1a878872980df525c7807c1b8b987c57d27f4bc3
anikape <ana.br@hotmail.com>
2022-09-09 14:02:59 -0300

    Projeto aula 03

---
+++ script.js
@@ -0,0 +1,41 @@
+let numeroSecreto = parseInt(Math.random() * 11);
+
+
+const tentativas = 5
+
+//o math.radom "sorteia" um número aleatório. Está multriplicado por 11 nossos números vão até 10
+
+
+
+
+function Chutar() {
+    let chute = parseInt(document.getElementById('valor').value); //transformando para número inteiro
+
+    let resultado = document.getElementById('resultado')
+
+    if (chute == numeroSecreto) {
+        resultado.innerHTML = "Parabéns! Você acertou! <a href='https://emojitool.com/pt/crystal-ball'>🔮</a>"
+
+    } else if (chute < 0 | chute > 10) {
+        resultado.innerHTML = "Você deve digitar um número entre 0 e 10. <br><br><br><a href='https://emojitool.com/pt/face-with-rolling-eyes'>🙄</a>";
+    } else {
+        resultado.innerHTML = "Você Errou!";
+    }
+
+
+}
+
+//contagem dos clicks
+
+//let count = 0;
+let btnChutar = document.getElementById('btnChutar');
+
+btnChutar.onclick = tentativas()
+
+function tentativas() {
+    count++;
+    if (count > 5) {
+
+        resultado.innerHTML = "Você excedeu o número de tentativas!";
+    }
+} //
